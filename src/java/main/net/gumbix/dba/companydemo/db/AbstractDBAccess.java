752cca322836e32eb59c722f8fe3e29d6cda3a8d
Markus Gumbel <m.gumbel@hs-mannheim.de>
2011-10-26 15:51:25 +0200

    Renamed

---
+++ src/java/main/net/gumbix/dba/companydemo/db/AbstractDBAccess.java
@@ -0,0 +1,37 @@
+package net.gumbix.dba.companydemo.db;
+
+import net.gumbix.dba.companydemo.domain.Employee;
+import net.gumbix.dba.companydemo.domain.Personnel;
+import net.gumbix.dba.companydemo.domain.Worker;
+
+/**
+ * @author Markus Gumbel (m.gumbel@hs-mannheim.de)
+ */
+abstract public class AbstractDBAccess implements DBAccess {
+
+    public Employee loadEmployee(long persNr) throws Exception {
+        // Note: An ObjectNotFound exception can be thrown anyway:
+        Personnel personnel = loadPersonnel(persNr);
+        if (personnel instanceof Employee) {
+            return (Employee) personnel;
+        } else {
+            throw new ObjectNotFoundException(personnel.getClass(),
+                    "Personnelnumber " +
+                            persNr + " refers to an object of type " +
+                            personnel.getClass().getName());
+        }
+    }
+
+    public Worker loadWorker(long persNr) throws Exception {
+        // Note: An ObjectNotFound exception can be thrown anyway:
+        Personnel personnel = loadPersonnel(persNr);
+        if (personnel instanceof Worker) {
+            return (Worker) personnel;
+        } else {
+            throw new ObjectNotFoundException(personnel.getClass(),
+                    "Personnelnumber " +
+                            persNr + " refers to an object of type " +
+                            personnel.getClass().getName());
+        }
+    }
+}

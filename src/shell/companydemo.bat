36e37ad4f2f2cc4a9d851adeac6bba2495873bd8
Markus Gumbel <m.gumbel@hs-mannheim.de>
2012-09-26 21:47:32 +0200

    Bug fixes!

--- src/shell/companydemo.bat
+++ src/shell/companydemo.bat
@@ -1,3 +1,5 @@
 @echo off
 echo Starting CompanyDemo...
-java -cp lib/companydemo-@{version}.jar;lib/db4o-8.0.184.15484-all-java5.jar;lib/mysql-connector-java-5.0.8-bin.jar net.gumbix.dba.companydemo.application.console.UI
+java -cp lib/companydemo-0.2.0.jar;resources;lib/antlr-2.7.6.jar;lib/commons-collections-3.1.jar;lib/companydemo-0.2.0.jar;lib/db4o-8.0.184.15484-all-java5.jar;lib/dom4j-1.6.1.jar;lib/hibernate3.jar;lib/hibernate-jpa-2.0-api-1.0.1.Final.jar;lib/javassist-3.12.0.GA.jar;lib/jta-1.1.jar;lib/junit-3.8.2.jar;lib/mysql-connector-java-5.0.8-bin.jar;lib/slf4j-api-1.6.1.jar net.gumbix.dba.companydemo.application.console.UI
+
+
